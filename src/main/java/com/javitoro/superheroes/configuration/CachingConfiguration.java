e2b6b8ce236cc81e4dfb85ff7f340cd8597adcc3
JaviToro <havitoro95@gmail.com>
2021-04-09 00:40:15 +0200

    Added cacheable requests

---
+++ src/main/java/com/javitoro/superheroes/configuration/CachingConfiguration.java
@@ -0,0 +1,28 @@
+package com.javitoro.superheroes.configuration;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class CachingConfiguration {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        List<Cache> caches = new ArrayList<Cache>();
+        caches.add(new ConcurrentMapCache("superheroes"));
+        caches.add(new ConcurrentMapCache("superhero"));
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+
+}
